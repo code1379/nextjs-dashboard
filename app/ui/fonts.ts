e92d18e336d597c925987e2196d88367eb74be93
code1379 <coderxialuo@gmail.com>
2024-10-25 11:34:22 +0800

    feat: chapter5 learn with official web

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// https://fonts.google.com/
+
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400" });
